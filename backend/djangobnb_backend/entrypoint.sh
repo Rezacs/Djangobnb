b9f1823c82c00386115b90774520e2eb5bdfa774
rezacs <ralmassizx@gmail.com>
2025-02-23 19:35:58 +0100

    good

---
+++ backend/djangobnb_backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Check if Database is running ..."
+
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+        sleep 0.1
+    done
+
+    echo "The database is up and running :-D"
+fi
+
+python manage.py makemigrations
+python manage.py migrate
+exec "$@"
---
+++ djangobnb/app/components/navbar/UserNav.tsx
@@ -0,0 +1,50 @@
+'use client';
+import MenuLink from "./MenuLink";
+
+import {useState} from "react" ;
+
+import useLoginModal from "@/app/hooks/useLoginModals";
+import useSignupModal from "@/app/hooks/useSignupModal";
+
+const UserNav = () => {
+    const loginModal = useLoginModal();
+    const signupModal = useSignupModal();
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <div className="p-2 relative inline-block border rounded-full">
+            <button 
+                className="flex items-center"
+                onClick={() => setIsOpen(!isOpen)}
+                >
+                <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                </svg>
+
+                <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+            </button>
+            {isOpen && (
+                <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+                    <MenuLink
+                        label = 'Log in'
+                        onClick={() =>{
+                            setIsOpen(false);
+                            loginModal.open();
+                        }}
+                    />
+                    <MenuLink
+                        label = 'Sign up'
+                        onClick={() =>{
+                            setIsOpen(false);
+                            signupModal.open();
+                        }}
+                    />
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default UserNav;
